42276a59dd40d7c52ac7f23e59b913a6fee4972f
Korya25 <enthrj3@gmail.com>
2025-05-28 12:52:25 +0300

    Update README.md with new content

---
+++ var_in_dart.dart
@@ -0,0 +1,24 @@
+void main() {
+  // var properties variables
+  String definitionVar =
+      'Used to define a variable with the type automatically inferred based on the initial value';
+  String varType = 'Static typing';
+  String changeTypeVar = 'Cannot change the type of the variable as (int)';
+
+  // dynamic properties variables
+  String definitionDynamic =
+      'Used to define a variable that can take any data type at any time';
+  String dynamicType = 'Dynamic typing';
+  String changeTypeDynamic =
+      'Can change the type of the variable as (int, String, bool, ...)';
+
+  // Display Difference
+  print('Var\n');
+  print(definitionVar);
+  print(varType);
+  print(changeTypeVar);
+  // Example in code
+  var name = 'Mahmoud';
+  name = 'Mohamed'; // Correct
+  // name = 10; // Error
+}
